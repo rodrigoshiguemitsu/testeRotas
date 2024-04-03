757acb9ce24ccab03de961ce85c5bd7a844e6485
Rodrigo Shiguemitsu <117852664+rodrigoshiguemitsu@users.noreply.github.com>
2024-04-02 22:32:05 -0300

    ...

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ export default function Rotas(){
     return(
     <BrowserRouter>
     <Routes>
-        
+        <Route path='/' element={<Home />}/>
         <Route path='/PaginaDois' element={<PagDois />}/>
         <Route path='/PaginaTres' element={<PagTres />}/>
         <Route path='*' element={<Home />}/>
