31183c0829731682aa1c7b4766f628b36e30f021
Rodrigo Shiguemitsu <117852664+rodrigoshiguemitsu@users.noreply.github.com>
2024-04-03 08:30:51 -0300

    ...

--- src/routes.js
+++ src/routes.js
@@ -9,22 +9,15 @@ import PagTres from './PagTres'
 
 export default function Rotas() {
     return (
-        // <BrowserRouter>
-        // <Routes>
-        //     <Route path='/' element={<Home />}/>
-        //     <Route path='/PaginaDois' element={<PagDois />}/>
-        //     <Route path='/PaginaTres' element={<PagTres />}/>
-        //     <Route path='*' element={<Home />}/>
-        // </Routes>
-        // </BrowserRouter>
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home />}>
-                <Route path="/PaginaDois" element={<PagDois />}/>
-                <Route path="/PaginaTres" element={<PagTres />} />
-            </Route>
+            <Route path='/' element={<Home />}/>
+            <Route path='/PaginaDois' element={<PagDois />}/>
+            <Route path='/PaginaTres' element={<PagTres />}/>
+           
         </Routes>
         </BrowserRouter>
+        
     )
 }
 
