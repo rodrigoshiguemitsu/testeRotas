efcb48607b6d012e55e5313039720c6bdd91f9e7
Rodrigo Shiguemitsu <117852664+rodrigoshiguemitsu@users.noreply.github.com>
2024-04-03 08:15:56 -0300

    ...

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './Home'
 import PagDois from './PagDois'
 import PagTres from './PagTres'
@@ -7,17 +7,25 @@ import PagTres from './PagTres'
 
 
 
-export default function Rotas(){
-    return(
-    
-    <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/PaginaDois' element={<PagDois />}/>
-        <Route path='/PaginaTres' element={<PagTres />}/>
-        <Route path='*' element={<Home />}/>
-    </Routes>
-    
-        )
+export default function Rotas() {
+    return (
+        // <BrowserRouter>
+        // <Routes>
+        //     <Route path='/' element={<Home />}/>
+        //     <Route path='/PaginaDois' element={<PagDois />}/>
+        //     <Route path='/PaginaTres' element={<PagTres />}/>
+        //     <Route path='*' element={<Home />}/>
+        // </Routes>
+        // </BrowserRouter>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />}>
+                <Route path="PaginaDois" element={<PagDois />}/>
+                <Route path="PaginaTres" element={<PagTres />} />
+            </Route>
+        </Routes>
+        </BrowserRouter>
+    )
 }
 
 
